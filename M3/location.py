58983c3973552f9d1f791ba0dc42d7d613c37ef9
George Raber <graber@gmail.com>
2018-02-02 10:53:55 -0600

    update with Google Key

--- M3/location.py
+++ M3/location.py
@@ -1,6 +1,6 @@
 import math, urllib
 def getaddresslocation(address):
-    params = urllib.urlencode({'address': address})
+    params = urllib.urlencode({'address': address, 'key': 'AIzaSyAIkAkn6l3NkAqQ1xXmIniNnGvL24_N1Lc'})
     f = urllib.urlopen("https://maps.googleapis.com/maps/api/geocode/json?%s" % params)
     loc = eval(f.read())
     return (loc["results"][0]["geometry"]["location"]['lng'],  loc["results"][0]["geometry"]["location"]['lat'])
