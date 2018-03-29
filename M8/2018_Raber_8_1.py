004d339287b68710cd2099804bc330b3a3e38f0d
George Raber <george.raber@usm.edu>
2018-03-29 17:02:17 -0500

    Middle of Lab 2018

--- M8/2018_Raber_8_1.py
+++ M8/2018_Raber_8_1.py
@@ -10,14 +10,19 @@ arcpy.AddField_management(fc, "PARA", "DOUBLE")
 arcpy.AddField_management(fc, "SSQR", "DOUBLE")
 arcpy.AddField_management(fc, "SCIRCLE", "DOUBLE")
 
-fields = ['PARA', 'SSQR', 'SCIRCLE', 'Name10', 'SHAPE@AREA', 'SHAPE@LENGTH']
+fields = ['PARA', 'SSQR', 'SCIRCLE', 'Name10', 'SHAPE@AREA', 'SHAPE@LENGTH', 'SHAPE@']
 
 cursor = arcpy.da.UpdateCursor(fc, fields)
 for row in cursor:
     row[0] = row[5] / row[4]
     row[1] = row[0] / (row[4]**0.5 / row[4])
-    #row[2] = row[0] /
+    #row[2] = row[0] / 
     cursor.updateRow(row)
+    points = []
+    for part in row[6]:
+        for pt in part:
+            print pt
+            points.append((pt.X, pt.Y))
     
 print "JOB COMPLETE!"
     
