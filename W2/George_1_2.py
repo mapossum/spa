967a0d94f0b3adb71e33d999fef1abac522e836c
George Raber <george.raber@usm.edu>
2021-01-27 13:52:48 -0600

    After Class Week 2 2021

---
+++ W2/George_1_2.py
@@ -0,0 +1,13 @@
+import math
+
+#This script will take two points and calcuate the distance between them.
+
+#Gather inputs
+x1 = 1
+y1 = 1
+
+x2 = 4
+y2 = 5
+
+
+#Find the distance between point 1 and point 2.
---
+++ W2/example4.py
@@ -0,0 +1,13 @@
+import math
+r = 6
+
+#This code below just demonstrates how to use some functions from the math module
+
+print(math.pow(4,3))
+print(math.sqrt(r))
+
+#This code just demos using a function as part of a larger statement
+num = math.pow(4,3) + r
+print(num)
+
+print(r**0.5)
