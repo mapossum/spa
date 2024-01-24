c72e5b13a85e4abf4d19e8c1490bf50ed002746f
George Raber <graber@gmail.com>
2024-01-24 15:20:31 -0600

    Update Raber_George_2_2.py

---
+++ W2/Raber_George_2_2.py
@@ -0,0 +1,14 @@
+#get the 4 values (x1,y1,x2 y2)
+
+#for example
+x1 = 0.0
+y1 = 0.0
+x2 = 3.0
+y2 = 4.0
+
+def findDistance(x1, x2, y1, y2):
+    #write code to find the distance between point 1 and point2
+
+    #write code to print the distance
+
+#write code to call that function
